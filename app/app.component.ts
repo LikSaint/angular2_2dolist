ea88f7ec0eff20c6d2828a927124bc6d7657eace
LikSaint <elnur1990@mail.ru>
2017-02-12 23:03:32 +0400

    'oneTemplate'

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
+class Todo {
+    constructor(public title: string, 
+                public completed: boolean = false) {}
+}
+
+const todos: Todo[] = [
+    {
+        title: 'Изучить JavaScript',
+        completed: true
+    },
+    {
+        title: 'Изучить Angular 2',
+        completed: false
+    },
+    {
+        title: 'Написать приложение',
+        completed: false
+    }
+];
+
 @Component({
+    moduleId: module.id,
     selector: 'app',
-    template: '<h1>Angular 2Do</h1>'
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
+
 })
 
 export class AppComponent {
+    title = 'Angular 2Do';
+    todos: Todo[]= todos;
+    newTodoTitle: string = '';
 
+    create() {
+        let todo: Todo = new Todo(this.newTodoTitle);
+        this.todos.push(todo);
+        this.newTodoTitle = '';
+    }
+
+    toggle(todo: Todo) {
+        todo.completed = !todo.completed;
+    }
+
+    delete(todo: Todo) {
+        let index = this.todos.indexOf(todo);
+
+        if (index > -1) {
+            this.todos.splice(index, 1);
+        }
+    }
 }
